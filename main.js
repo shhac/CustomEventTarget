c8248915ee3ff4258e6d4d60858dda365fd73125
Paul Somers <paulsomers@gmail.com>
2017-07-19 18:07:04 +0100

    npm-ify
    * WeakMap is being set in env
    * Map is being set in env

---
+++ main.js
@@ -0,0 +1,8 @@
+require('es6-symbol/implement');
+require('es6-map/implement');
+require('es6-weak-map/implement');
+
+require('import-export');
+
+module.exports = require('./CustomEventTarget.js');
+
